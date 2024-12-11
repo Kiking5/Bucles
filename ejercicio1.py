d2fd13bd59c1666879619bb863b9a9dbe16394f7
Johnattan Peña <167146327+Kiking5@users.noreply.github.com>
2024-12-11 11:00:42 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,7 @@
+"""Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces."""
+
+palabra = input("Ingrese una palabra: ")
+contador = 0
+while contador < 10:
+    print(palabra)
+    contador += 1
